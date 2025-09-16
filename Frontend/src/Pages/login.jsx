3ca91ae082961a9d9ca1d7526ae8d6fe2c6b0d53
Santino Scampone Garcia <santinoscampone1@gmail.com>
2025-09-16 20:32:34 -0300

    Empezado el loguin

---
+++ Frontend/src/Pages/login.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Typography, Button, Box, AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemText, ListItemIcon, ListItemButton, Divider, Stack, Link, TextField } from "@mui/material";
+import axios from 'axios';
+
+function Login() {
+    return (
+        <Typography>Estoy en el login</Typography>
+    );
+};
+
+export default Login;
